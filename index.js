b983b927b8b20cfd87e736acf102bb5042e98d29
jonschlinkert <jon.schlinkert@sellside.com>
2015-02-13 23:48:54 -0500

    lint

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*!
- * omit-keys <https://github.com/jonschlinkert/omit-keys>
+ * object.omit <https://github.com/jonschlinkert/object.omit>
  *
- * Copyright (c) 2014 Jon Schlinkert, contributors.
+ * Copyright (c) 2014-2015 Jon Schlinkert.
  * Licensed under the MIT License
  */
 
