b23b2fd79473518dd81ce75a9a6bb64b7942abee
jonschlinkert <jon.schlinkert@sellside.com>
2015-07-22 02:20:04 -0400

    support filter function as last arg

--- test.js
+++ test.js
@@ -24,6 +24,27 @@ describe('.omit()', function () {
     omit({a: 'a', b: 'b', c: 'c'}).should.eql({a: 'a', b: 'b', c: 'c'});
   });
 
+  it('should take a filter function as the last argument.', function () {
+    var foo = omit({a: 'a', b: 'b', c: 'c'}, function (val, key) {
+      return key === 'a';
+    });
+    var fn = function() {};
+    var bar = omit({a: 'a', b: 'b', c: fn}, function (val, key) {
+      return typeof val !== 'function';
+    });
+    foo.should.eql({a: 'a'});
+    bar.should.eql({a: 'a', b: 'b'});
+  });
+
+  it('should copy properties to a new object.', function () {
+    var foo = {a: 'a', b: 'b', c: 'c', d: 'd'};
+    var bar = omit(foo, ['d'], function (val, key) {
+      return key === 'a' || key === 'd';
+    });
+    foo.should.eql({a: 'a', b: 'b', c: 'c', d: 'd'});
+    bar.should.eql({a: 'a'});
+  });
+
   it('should return an empty object if the first arg is not an object.', function () {
     omit(null, {a: 'a', b: 'b', c: 'c'}).should.eql({});
   });
