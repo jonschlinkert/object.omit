75cca2a1cd736ab56207f944c194b500203b8179
jonschlinkert <jon.schlinkert@sellside.com>
2017-09-03 23:44:37 -0400

    update unit tests
    - remove tests related to code that was removed
    - add tests to ensure a new object is always returned, for consistency

--- test.js
+++ test.js
@@ -7,58 +7,54 @@
 
 'use strict';
 
-/* deps:mocha */
-var should = require('should');
+require('mocha');
+var assert = require('assert');
 var omit = require('./');
 
-describe('.omit()', function () {
-  it('should omit the given key from the object.', function () {
-    omit({a: 'a', b: 'b', c: 'c'}, 'a').should.eql({ b: 'b', c: 'c' });
+describe('.omit()', function() {
+  it('should omit a key from the object', function() {
+    assert.deepEqual(omit({a: 'a', b: 'b', c: 'c'}, 'a'), { b: 'b', c: 'c' });
+    assert.deepEqual(omit({aaa: 'a', bbb: 'b', ccc: 'c'}, 'aaa'), { bbb: 'b', ccc: 'c' });
   });
 
-  it('should omit the given keys as array from the object.', function () {
-    omit({a: 'a', b: 'b', c: 'c'}, ['a', 'c']).should.eql({ b: 'b' });
+  it('should omit an array of keys from the object', function() {
+    assert.deepEqual(omit({a: 'a', b: 'b', c: 'c'}, ['a', 'c']), { b: 'b' });
   });
 
-  it('should omit the given keys as arrays from the object.', function () {
-    omit({a: 'a', b: 'b', c: 'c'}, ['a'], ['b', 'c']).should.eql({});
+  it('should return the object if no keys are given', function() {
+    assert.deepEqual(omit({a: 'a', b: 'b', c: 'c'}), {a: 'a', b: 'b', c: 'c'});
   });
 
-  it('should omit the given keys as parameters from the object.', function () {
-    omit({a: 'a', b: 'b', c: 'c'}, 'a', 'c').should.eql({ b: 'b' });
+  it('should return a new object when no keys are given', function() {
+    var obj = {a: 'a', b: 'b', c: 'c'};
+    assert(omit(obj) !== obj);
   });
 
-
-  it('should return the object if no keys are specified.', function () {
-    omit({a: 'a', b: 'b', c: 'c'}).should.eql({a: 'a', b: 'b', c: 'c'});
-  });
-
-  it('should take a filter function as the last argument.', function () {
-    var foo = omit({a: 'a', b: 'b', c: 'c'}, function (val, key) {
+  it('should take a filter function as the last argument', function() {
+    var foo = omit({a: 'a', b: 'b', c: 'c'}, function(val, key) {
       return key === 'a';
     });
-    var fn = function() {};
-    var bar = omit({a: 'a', b: 'b', c: fn}, function (val, key) {
+    var bar = omit({a: 'a', b: 'b', c: function() {}}, function(val, key) {
       return typeof val !== 'function';
     });
-    foo.should.eql({a: 'a'});
-    bar.should.eql({a: 'a', b: 'b'});
+    assert.deepEqual(foo, {a: 'a'});
+    assert.deepEqual(bar, {a: 'a', b: 'b'});
   });
 
-  it('should copy properties to a new object.', function () {
+  it('should copy properties to a new object', function() {
     var foo = {a: 'a', b: 'b', c: 'c', d: 'd'};
-    var bar = omit(foo, ['d'], function (val, key) {
+    var bar = omit(foo, ['d'], function(val, key) {
       return key === 'a' || key === 'd';
     });
-    foo.should.eql({a: 'a', b: 'b', c: 'c', d: 'd'});
-    bar.should.eql({a: 'a'});
+    assert.deepEqual(foo, {a: 'a', b: 'b', c: 'c', d: 'd'});
+    assert.deepEqual(bar, {a: 'a'});
   });
 
-  it('should return an empty object if the first arg is not an object.', function () {
-    omit(null, {a: 'a', b: 'b', c: 'c'}).should.eql({});
+  it('should return an empty object if the first arg is not an object', function() {
+    assert.deepEqual(omit(null, {a: 'a', b: 'b', c: 'c'}), {});
   });
 
-  it('should return an empty object if no object is specified.', function () {
-    omit().should.eql({});
+  it('should return an empty object if no object is specified', function() {
+    assert.deepEqual(omit(), {});
   });
 });
